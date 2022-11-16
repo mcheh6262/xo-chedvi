63b1aa6a5a3a251b5b63c0c755af674b5c54c150
mcheh6262 <112472048+mcheh6262@users.noreply.github.com>
2022-11-16 22:03:17 +0200

    Update eiqs.js

--- lesson 17/eiqs.js
+++ lesson 17/eiqs.js
@@ -2,22 +2,25 @@ const eiqseiguel = [undefined,undefined,undefined,undefined,undefined,undefined,
 let player = "0"
 let computer = "x"
 let miqom = 0
+let won_player = 'player won the game'
+let won_computer = 'computer won the game'
+let tiko = 'tiko'
+game_ove = false
 
 play()
 function play() {
     for(let i in eiqseiguel) {
         eiqseiguel[miqom] = undefined
     }
-    document.getElementById('won').innerHTML = undefined
+    document.getElementById('won').innerHTML = ""
     for (let i = 0; i <=8; i++) {
-        document.getElementById(`box${i}`).innerHTML = undefined
+        document.getElementById(`box${i}`).innerHTML = ""
     }
     for (let i = 0; i <=8; i++) {
         document.getElementById(`box${i}`).disabled = false
     }
     document.getElementById(`play`).disabled = false
 }
-
 
 function div0() {
     if (eiqseiguel[0] == undefined) {
@@ -94,7 +97,7 @@ function div8() {
     }  
 }
 
-function tnaim(x) {
+function tnaim(won) {
 if (eiqseiguel[0] === eiqseiguel[1] && eiqseiguel[0] === eiqseiguel[2] === won ||
     eiqseiguel[3] === eiqseiguel[4] && eiqseiguel[3] === eiqseiguel[5] === won ||
     eiqseiguel[6] === eiqseiguel[7] && eiqseiguel[6] === eiqseiguel[8] === won ||
@@ -104,14 +107,29 @@ if (eiqseiguel[0] === eiqseiguel[1] && eiqseiguel[0] === eiqseiguel[2] === won |
     eiqseiguel[0] === eiqseiguel[4] && eiqseiguel[0] === eiqseiguel[8] === won ||
     eiqseiguel[2] === eiqseiguel[4] && eiqseiguel[2] === eiqseiguel[6] === won 
 ) {
-document.getElementById('won').innerHTML = x + "won!"
+    
+    return true;
+
+    }
+    else {
+        return false;  
+    }
+} 
+    if (tnaim(player)) { 
+        $(`#'won'`).text(won_player)
+        !game_ove
+        
+    }
+    else if (!tnaim(player) && !tnaim(computer)) { 
+        $(`#won`).text(tiko)
+        !game_ove
+    } 
+    else if (tnaim(computer)) {
+        $(`#won`).text(won_computer)
+        !game_ove
+    }
+
 game_over()
-  }
-
-}
-
-
-
 function game_over() {
     for (let i = 0; i <= 8; i++) {
 document.getElementById(`$box{i}`).disable = true 
@@ -120,6 +138,8 @@ document.getElementById(`play`).disable = false
 }
 
 function computer1() {
+    if (eiqseiguel.every(e => e != undefined))
+    return;
 do {miqom = Math.floor (Math.random()*9)} 
 while(eiqseiguel[miqom] !== undefined)
 if(eiqseiguel[miqom] == undefined) {
@@ -129,5 +149,4 @@ if(eiqseiguel[miqom] == undefined) {
   }
 
 }
-
 
